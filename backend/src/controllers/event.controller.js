6715391d42de7229870e71876b51427082e7badb
saquibjawedbit <saquibjawed4444@gmail.com>
2025-09-11 18:04:35 +0530

    feat: Integrate Redis caching for event retrieval and management, added update and delete event routes

--- backend/src/controllers/event.controller.js
+++ backend/src/controllers/event.controller.js
@@ -1,18 +1,29 @@
 import { PrismaClient } from "@prisma/client";
+import redis from "../configs/redis.js";
 
 const prisma = new PrismaClient();
 
+// Get all events with caching
 export const getAllEvents = async (req, res) => {
   try {
+
+    const cached = await redis.get("events:all");
+    if (cached) {
+      return res.json({ source: "cache", data: JSON.parse(cached) });
+    }
+
     const events = await prisma.event.findMany({
       include: { bookings: true },
     });
+
+    await redis.setex("events:all", 60, JSON.stringify(events)); // 60 seconds
     res.json(events);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
+// Create a new event
 export const createEvent = async (req, res) => {
   const { name, venue, time, capacity } = req.body;
   try {
@@ -20,6 +31,47 @@ export const createEvent = async (req, res) => {
       data: { name, venue, time: new Date(time), capacity },
     });
     res.json(event);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// Update an existing event
+export const updateEvent = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, venue, time, capacity } = req.body;
+
+    const event = await prisma.event.update({
+      where: { id: Number(id) },
+      data: {
+        name,
+        venue,
+        time: new Date(time),
+        capacity,
+      },
+    });
+
+    await redis.del("events:all");
+
+    res.json(event);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+
+export const deleteEvent = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await prisma.event.delete({
+      where: { id: Number(id) },
+    });
+
+    await redis.del("events:all");
+
+    res.json({ message: "Event deleted successfully" });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
--- backend/src/routes/event.routes.js
+++ backend/src/routes/event.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getAllEvents, createEvent } from "../controllers/event.controller.js";
+import { getAllEvents, createEvent, updateEvent, deleteEvent } from "../controllers/event.controller.js";
 import { authMiddleware } from "../middleware/auth.js";
 
 const router = Router();
 
 router.get("/view", getAllEvents);
 router.post("/create", authMiddleware(["ADMIN"]), createEvent);
+router.put("/:id", authMiddleware(["ADMIN"]), updateEvent);
+router.delete("/:id", authMiddleware(["ADMIN"]), deleteEvent);
 
 export default router;
