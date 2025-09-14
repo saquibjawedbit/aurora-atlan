2fbeb74400893c00f2daf8555157806ab29055e4
Md Saquib Jawed <saquibjawed4444@gmail.com>
2025-09-14 20:50:38 +0530

    feat: add Postman collection for Event Booking API and update Redis config for authentication

--- backend/src/configs/redis.js
+++ backend/src/configs/redis.js
@@ -7,6 +7,8 @@ dotenv.config();
 const redis = new Redis({
   host: process.env.REDIS_HOST || "127.0.0.1",
   port: process.env.REDIS_PORT || 6379,
+  username: process.env.REDIS_USERNAME || null,
+  password: process.env.REDIS_PASSWORD || null,
   retryDelayOnFailover: 100,
   enableReadyCheck: false,
   maxRetriesPerRequest: null,
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -18,6 +18,7 @@ export const register = async (req, res) => {
 
     res.json({ user, token: generateToken(user) });
   } catch (err) {
+    console.log(err);
     res.status(500).json({ error: err.message });
   }
 };
